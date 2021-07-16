db5a7956a309991132ba9296f3a95ca4796e435c
hemamalniP <87513027+hemamalniP@users.noreply.github.com>
2021-07-16 11:33:29 +0530

    que14.py
    There are two arrays 
           A = (1,2,3,4,5) the result should be 
           B = (120,60,40,30,24)
            120 = 2*3*4*5
              60 = 1*3*4*5
              40 = 1*2*4*5
              30 = 1*2*3*5
              24 = 1*2*3*4

---
+++ productarray.py
@@ -0,0 +1,24 @@
+def product(arr, n):
+  
+    if n == 1:
+        print(0)
+        return
+    i=1
+    temp=1
+    prod = [1 for i in range(n)]
+    for i in range(n):
+        prod[i] = temp
+        temp *= arr[i]
+    temp = 1
+    for i in range(n - 1, -1, -1):
+        prod[i] *= temp
+        temp *= arr[i]
+    for i in range(n):
+        print(prod[i], end = " ")
+  
+    return
+  
+arr = [1,2,3,4,5]
+n = len(arr)
+print("product array")
+product(arr, n)
