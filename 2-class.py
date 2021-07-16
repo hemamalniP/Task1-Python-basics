d5753cbc51d96f5cca9f7c0f6d2443e02adb2037
hemamalniP <87513027+hemamalniP@users.noreply.github.com>
2021-07-16 11:18:01 +0530

    que2.py
    Define a class which has at least two methods:
    getString: to get a string from console input
    printString: to print the string in upper case.

---
+++ 2-class.py
@@ -0,0 +1,11 @@
+class str():
+    def init(self):
+        self.str1=""
+    def get(self):
+        self.str1=input()
+    def print(self):
+        print(self.str1.upper())
+
+str1=str()
+str1.get()
+str1.print()
